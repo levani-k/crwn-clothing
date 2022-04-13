2992c3aff65d854520f4c57d4ced172f33b3457b
levani-k <levaniqistauri@gmail.com>
2022-04-13 10:57:12 +0400

    chore: use reduc logger instead of custom logger and add redux devtools support

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
-
-  next(action);
-
-  console.log("new state: ", store.getState());
-};
 
 const persistConfig = {
   key: "root",
@@ -27,9 +13,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [loggerMiddleware];
+const middlewares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const composeEnhancer =
+  (process.env.NODE_ENV === "development" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
 export const store = createStore(
   persistedReducer,
