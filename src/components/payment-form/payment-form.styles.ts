f36265527cb4149a00241cc74859b6ad0b2d3887
levani-k <levaniqistauri@gmail.com>
2022-06-01 21:15:19 +0400

    style: add some styles for mobile view

--- src/components/payment-form/payment-form.styles.ts
+++ src/components/payment-form/payment-form.styles.ts
@@ -7,11 +7,22 @@ export const PaymentFormContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+
+    @media screen and (max-width: 800px) {
+        height: 200px;
+        width: 100%;
+        margin-bottom: 50px;
+    }
 `
 
 export const FormContainer = styled.form`
     height: 100px;
     min-width: 500px;
+
+    @media screen and (max-width: 800px) {
+        min-width: 300px;
+        max-width: 100%;
+    }
 `
 
 export const PaymentButton = styled(Button)`
